1bbe1173e32149cf9cf2a5dbdf8b326e962fbacc
Richie Ito <ricito4@gmail.com>
2016-05-25 18:58:46 -0400

    Add conditional logic in vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,43 @@
 def questioning
-	puts "What is your name?"
-	name= gets.chomp
-	puts "How old are you?"
-	age= gets.to_i
-	puts "What year were you born?"
-	year= gets.to_i
-	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-	bread= gets.chomp
-	puts= "Would you like to enroll in the company’s health insurance?"
-	insure= gets.chomp
+	puts "How many times should the survey be taken?"
+	times= gets.to_i
+
+		puts "What is your name?"
+		name= gets.chomp
+		puts "How old are you?"
+		age= gets.to_i
+		puts "What year were you born?"
+		year= gets.to_i
+		puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes or No)"
+		bread= gets.chomp
+		puts= "Would you like to enroll in the company’s health insurance? (Yes or No)"
+		insure= gets.chomp
+
+		response= "Results inconclusive"
+		current_year= 2016
+		
+		# 2 conditions for age since maybe their birthday has not occured this year
+		# Condition 1
+		if (current_year-age = year || current_year-age = year+1) && (bread== "Yes" || insure== "Yes")
+			response = "Probably not a vampire."
+		end
+		
+		# Condition 2
+		if (current_year-age != year || current_year-age != year+1) && (bread== "No" || insure== "No")
+			response = "Probably a vampire."
+		end
+
+		# Condition 3
+		if (current_year-age != year || current_year-age != year+1) && (bread== "No" && insure== "No")
+			response = "Almost certainly a vampire."
+		end
+
+		# Condition 4
+		if name == "Drake Cula" || name== "Tu Fang"
+			response = "Definitely a vampire."
+		end
+		puts name + ": " + response
+
 end
 
 questioning
