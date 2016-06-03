202d3a0563d66426ad9469fcc2f9a310eeefab94
Richie Ito <ricito4@gmail.com>
2016-06-02 21:04:31 -0400

    Add simple data structures and modify them using each and map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -10,3 +10,30 @@ def test_method
 end
 
 test_method { |name| puts "Your name is #{name}"}
+
+
+array_movies = ["Top Gun", "Transformers", "Lego Movie", "Mean Girls"]
+
+hash_actors= {"Tom Cruise" => "Maverick", "Shia LeBuff" => "Transformers Dude", "Morgan Freeman" =>"Oracle", "Lindsey Lohan" => "Nice Girl"}
+
+array_movies.each do |movie|
+	puts movie
+end
+
+hash_actors.each do |actors,role|
+	puts "#{actors}'s best role was #{role}"
+end
+
+array_movies.map! do |movie|
+	movie+" 2"
+end
+puts array_movies
+puts hash_actors
+
+delete_if { |item| block } → ary
+keep_if { |item| block } → ary
+select { |item| block } → new_ary
+
+delete_if {| key, value | block } → hsh
+keep_if {| key, value | block } → hsh
+select {|key, value| block} → a_hash
