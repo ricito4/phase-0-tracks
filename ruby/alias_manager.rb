172b7617451c879f1fab442c650cab599d746545
Richie Ito <ricito4@gmail.com>
2016-06-05 00:59:15 -0400

    Add initial iteration method. Change psudo code and approach to iteration

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,9 @@
 # First ask user for name, validate that there are spaces
 # Swap first and last names
 # Split both first and last names up into arrays with each letter as 1 element
-# Take each element and convert it to lower case, if the change is made then store a temp variable
-# Compare each letter in the array to either a consonant array or vowel array
+# Compare each letter in the array to a lower case consonant or vowel array
+# if it returns nil then compare it to an upper case consonant or vowell array
 # Change the letter to the next letter, wraps around if it is the last letter
-# If the temp variable is changed change character to upper
 # after looping combine the array
 # return array
 
@@ -13,6 +12,7 @@ full_name= gets.chomp
 p name_array= full_name.split(' ')
 number= name_array.length
 
+# Method to advance each name 1 index to the left and wrap around
 for i in 0..number-1
 	if i < number-1
 		temp= name_array[i]
@@ -21,6 +21,14 @@ for i in 0..number-1
 	end
 end
 
+upper_vowel= "AEIOU"
+lower_vowel= "aeiou"
+upper_consonant="BCDFGHJKLMNPQRSTVWXYZ"
+lower_consonant="bcdfghjklmnpqrstvwxyz" 
+
 name_array.map! do |eachname|
-	p current_name=eachname.split('')
+	current_name=eachname.split('')
+	index= current_name.length
+
+
 end
