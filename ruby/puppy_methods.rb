3e870ee59e3cd8c481d243c5799d7e158144ff4e
Richie Ito <ricito4@gmail.com>
2016-06-07 18:39:46 -0400

    Add comments of how we would construct a puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,16 @@
+# Name-----
+# Species:
+
+# Characteristics ---
+# height:
+# weight:
+# color:
+
+# Behavior ---
+# is it playful:
+# food it likes:
+# food it dislikes:
+
 class Puppy
 
   def fetch(toy)
