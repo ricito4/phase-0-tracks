8800b531cdb39d74327abc66d506a2d4152ef500
Richie Ito <ricito4@gmail.com>
2016-06-09 09:55:08 -0400

    Add a method that asks for user input for an instance of pokemon

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -91,3 +91,106 @@ end
 # mewtwo.i_choose_you
 # mewtwo.level_up
 # mewtwo.learn_new_move("Psychic")
+
+def pokemon_creator
+
+	# ask for which pokemon they want
+	valid_pokemon= false
+	puts "Which Pokemon would you like?"
+	until valid_pokemon
+		chosen_pokemon= gets.chomp
+		if chosen_pokemon.empty?
+			puts "Invalid response, Please enter a Pokemon"
+		else
+			valid_pokemon= true
+		end
+	end
+
+	# ask if they want a nickname for it
+	valid_want_nickname= false
+	puts "Would you like to choose a nickname? (yes/no)"
+	until valid_want_nickname
+		want_nickname= gets.chomp
+		
+		if want_nickname.downcase== "yes"
+			valid_want_nickname= true
+			puts "Please type in what nickname you would like to give"
+			valid_nickname= false
+			until valid_nickname
+				chosen_nickname= gets.chomp
+				if chosen_nickname.empty?
+					puts "Invalid response"
+				else
+					valid_nickname= true
+				end
+			end
+
+		elsif want_nickname.downcase== "no"
+			valid_want_nickname= true
+			chosen_nickname= chosen_pokemon
+		else
+			puts "Invalid response"
+		end
+	end
+
+	# ask for what level they want their pokemon to be
+	valid_level= false
+	puts "What level do you want your pokemon at? (1-100)"
+	until valid_level
+		chosen_level= gets.to_i
+		if chosen_pokemon.empty?
+			puts "Invalid response, Please select a level"
+		elsif chosen_level < 1
+			puts "Invalid response, This pokemon must be at least level 1"
+		elsif chosen_level > 100
+			puts "Invalid response, This pokemon's level cannot exceed 100"
+		else	
+			valid_level= true
+		end
+	end
+
+	# ask for this pokemon's moveset
+	chosen_moveset=[]
+	moveset_finish= false
+	
+	until moveset_finish
+		valid_move= false
+		puts "Please add a move you want this pokemon to know"
+		until valid_move
+			chosen_move= gets.chomp
+			if chosen_move.empty?
+				puts "Invalid response, Please enter a move"
+			else
+				valid_move= true
+				chosen_moveset.push(chosen_move)
+			end
+		end
+		
+		puts "#{chosen_pokemon}'s current movset is #{chosen_moveset}"
+		if chosen_moveset.length < 4
+			puts "Would you like to add another move? (yes/no)"
+			valid_yes_no= false
+			until valid_yes_no
+				more_moves= gets.chomp
+				if more_moves.downcase == "no"
+					valid_yes_no= true
+					moveset_finish= true
+				elsif more_moves.downcase == "yes"
+					valid_yes_no= true
+				else
+					puts "Invalid response"
+				end
+			end
+				
+
+		else
+			puts "#{chosen_pokemon} cannot learn any more moves"
+			moveset_finish= true
+		end
+	end
+
+	instance_pokemon= Pokemon.new(chosen_pokemon,chosen_nickname,chosen_level,chosen_nickname)
+
+end
+
+pokemon_creator
