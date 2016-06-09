06d6e837ff420b49ea8d7e33e936a683ecb23616
Richie Ito <ricito4@gmail.com>
2016-06-09 07:44:45 -0400

    Add psuedo code for solo.rb

---
+++ ruby/solo.rb
@@ -0,0 +1,12 @@
+# Make a class Pokemon
+# Have the class with attributes: 
+# Which pokemon it is,
+# A nickname for the pokemon, if no input, default = the pokemon name
+# What level the pokemon is from 1-100
+# A array of up to 4 strings that represent pokemon moves, driver code will 
+# convert user input to 1 string
+
+# A method that chooses the pokemon by calling its nickname
+# A method that levels up the pokemon, if it is max level return that it is at max level
+# A method to learn a new move, if the moveset is full ask if they want to 
+# replace one of the moves in the moveset for the new move.
