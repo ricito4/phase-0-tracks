ea05ff34eedd432a0861c8190f8ae7d5c0d887e9
Richie Ito <ricito4@gmail.com>
2016-06-09 13:40:02 -0400

    Add shout module method and call it. Add module to flight.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	def self.yell_angrily(words)
     	words + "!!!" + " :("
@@ -8,5 +9,27 @@ module Shout
   	end
 end
 
+
 p Shout.yell_angrily("OUCH")
 p Shout.yelling_happily("FREE FOOD")
+=end
+
+module Shout
+	def shouter(words)
+    	puts "#{words}!!!"
+	end
+end
+
+class Speaker
+	include Shout
+end
+
+class Old_Man
+	include Shout
+end
+
+shia_labeouf= Speaker.new
+shia_labeouf.shouter("JUST DO IT")
+
+mr_johnson= Old_Man.new
+mr_johnson.shouter("Get off my lawn")
