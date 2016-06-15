7234e80aa547e2f81b667748be4210c7affc8146
Richie Ito <ricito4@gmail.com>
2016-06-15 17:49:18 -0400

    Write pseudo code for a reverse string method and write the syntax for it

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,21 @@
+// A function that reverses a string
+// loop through the string until 1/2 it's length
+// store the first letter in a variable
+// reassign the first letter as the last letter in the string
+// assign the last letter in the string as the variable
+
+
 function reverse(string) {
-	for (var i = 0; i < string.length; i++) {
-		console.log("hello");
+	
+	str= string.split('');
+	for (var i = 0; i < str.length/2; i++) {
+		tempVar= str[i];
+		str[i]= str[str.length-i-1];
+		str[str.length-i-1]= tempVar;
+	}
+	return str.join('');
 
 }
 
+string1 = reverse("abcdef");
+console.log (string1)
