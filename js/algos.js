994b33aa0e2cfc37b6c321c96b4ba50d28eb7ec1
Richie Ito <ricito4@gmail.com>
2016-06-17 04:54:16 -0400

    Initialize algos.js and add pseudo code for the first method

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+// first get length of the the array
+// set up a for loop that will go to length - 1
+// set up an initial variable for the index of the longest string as the 0th index
+// iterate through the array comparing the ith and ith+1 element in the array to see which is longer using the .length method
+// if the ith + 1 is bigger than the ith then set the index to the ith+1
+// when finished return the string at the stored index
